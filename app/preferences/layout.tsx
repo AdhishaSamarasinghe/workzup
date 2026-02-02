96dbf425b7d9ccb450e2c143ccb636f62a62e948
Chamalsha Induwara <chamalsha.20240765@iit.ac.lk>
2026-02-02 22:54:02 +0530

    Remove Footer from preferences layout
    The Footer component has been removed from the preferences layout to simplify the page structure or address layout requirements.

--- app/preferences/layout.tsx
+++ app/preferences/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <div className="flex min-h-screen flex-col">
           <SkillsHeader />
           <main className="flex-1">{children}</main>
-          <Footer />
         </div>
       </body>
     </html>
