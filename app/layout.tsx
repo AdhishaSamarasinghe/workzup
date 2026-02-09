e89c360fb9c4f2739085af6292d670b30862abb0
Ishara Hasanjan <ishara.20232856@iit.ac.lk>
2026-02-09 11:54:25 +0530

    Revert "Static pages"

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ type RootLayoutProps = {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-bg text-[#111827] antialiased" suppressHydrationWarning>
+      <body className="min-h-screen bg-bg text-[#111827] antialiased">
         <div className="flex min-h-screen flex-col">
           <Header />
           <main className="flex-1">{children}</main>
